740cb0cacb57bbaca9cab88d8dd0609fc87649bf
nrnbt <nrnbt0616@gmail.com>
2022-11-29 01:43:41 +0800

    admin update

---
+++ src/main/java/admin/Auction.java
@@ -0,0 +1,41 @@
+package admin;
+
+import java.io.Serializable;
+
+public class Auction implements Serializable {
+    public int id;
+    public String title;
+    public String user;
+    public String startPrice;
+    public String endPrice;
+    public String startTime;
+    public String endTime;
+    public String status;
+    public String img;
+    public String winner;
+    public String description;
+
+    public Auction(int id,
+            String title,
+            String user,
+            String startPrice,
+            String endPrice,
+            String startTime,
+            String endTime,
+            String status,
+            String img,
+            String winner,
+            String description) {
+        this.id = id;
+        this.title = title;
+        this.startPrice = startPrice;
+        this.endPrice = endPrice;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.status = status;
+        this.img = img;
+        this.winner = winner;
+        this.description = description;
+    }
+
+}
